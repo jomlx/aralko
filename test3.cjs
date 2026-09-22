7cb51565bb47f84c7c3301b9af0b2d86fbce0815
jomlx <mislos.jomel@gmail.com>
2026-09-22 09:06:35 +0800

    chore: remove temp test scripts

--- test3.cjs
+++
@@ -1,1 +0,0 @@
-require('dotenv').config(); const { createClient } = require('@supabase/supabase-js'); const supabase = createClient(process.env.VITE_SUPABASE_URL, process.env.VITE_SUPABASE_ANON_KEY); supabase.from('user_settings').select('username').limit(1).then(console.log).catch(console.error);
